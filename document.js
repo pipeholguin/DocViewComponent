ba2ed4ea96cda742af1a17cd5fed5b24c88353d0
Luis Felipe Holguin <luis@farleyetc.com>
2016-10-28 17:28:36 -0500

    todo

---
+++ document.js
@@ -0,0 +1,11 @@
+var vHeight = 480;
+var vWidth = 600;
+$(document).ready(function () {
+  $('#video').css({
+    height: vHeight + 'px'
+  });
+  $('#video').css({
+    width: vWidth + 'px'
+  });
+
+});
